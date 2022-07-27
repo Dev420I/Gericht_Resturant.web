458f01167b4b2cc2d6e15300df0a315f583ee218
daviddev <david.arutyunyan.@bk.ru>
2022-07-27 10:41:00 +0300

    patch 5.1

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -13,10 +13,10 @@ const SpecialMenu = () => (
 
       <div className='app__specialMenu-menu'>
           <div className='app__specialMenu-menu_wine flex__center>'>
-              <p className='app__specialMenu_heading'>Wine & Beer</p>
+              <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
               <div className='app__specialMenu_menu_items'>
                   {data.wines.map((wine, index) => (
-                      <p>{wine.title}</p>
+                      <MenuItem key={wine.title + index} title={wine.title}  price={wine.price} tags={wine.tags}/>
                   ))}
               </div>
           </div>
@@ -26,10 +26,10 @@ const SpecialMenu = () => (
           </div>
 
           <div className='app__specialMenu-menu_cocktails flex__center>'>
-              <p className='app__specialMenu_heading'>Cocktails</p>
+              <p className='app__specialMenu-menu_heading'>Cocktails</p>
               <div className='app__specialMenu_menu_items'>
                   {data.cocktails.map((cocktail, index) => (
-                      <p>{cocktail.title}</p>
+                      <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags}/>
                   ))}
               </div>
           </div>
