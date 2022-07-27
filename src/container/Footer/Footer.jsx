0be1fadf60e6d40a7b1acfcd2d74437442be9acd
daviddev <david.arutyunyan.@bk.ru>
2022-07-27 23:58:09 +0300

    patch 8

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -8,6 +8,21 @@ const Footer = () => (
   <div className='app__footer section__padding'>
     <FooterOverlay />
       <Newsletter/>
+
+      <div className='app__footer-links'>
+          <div className='app__footer-links_contact'>
+             <h1 className='app__footer-headtext'>Contact Us</h1>
+          </div>
+          <div className='app__footer-links_logo'>
+
+          </div>
+          <div className='app__footer-links_work'>
+
+          </div>
+      </div>
+      <div className='footer__copyright'>
+          <p className='p__opensans'>2021 Gerícht. All Rights reserved.</p>
+      </div>
   </div>
 );
 
