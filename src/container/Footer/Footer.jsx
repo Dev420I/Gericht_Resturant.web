0c06a04109f2d916261aa9546d89b683a7532ea0
daviddev <david.arutyunyan.@bk.ru>
2022-07-28 16:12:26 +0300

    patch 9

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {FiFecebook, FiTwiter, FiInstagram} from "react-icons/fi";
 import {FooterOverlay, Newsletter,} from '../../components'
 import './Footer.css';
+import {images} from "../../constants";
 
 const Footer = () => (
   <div className='app__footer section__padding'>
@@ -12,16 +13,18 @@ const Footer = () => (
       <div className='app__footer-links'>
           <div className='app__footer-links_contact'>
              <h1 className='app__footer-headtext'>Contact Us</h1>
+              <p className='p__opensans'> https://github.com/Dev420I</p>
+              <p className='p__opensans'>+7 977-491-64-39</p>
           </div>
           <div className='app__footer-links_logo'>
-
+              <img src={images.gericht} alt='footer_logo'/>
           </div>
           <div className='app__footer-links_work'>
 
           </div>
       </div>
       <div className='footer__copyright'>
-          <p className='p__opensans'>2021 Gerícht. All Rights reserved.</p>
+          <p className='p__opensans'>2022 Gerícht.https://github.com/Dev420I.</p>
       </div>
   </div>
 );
